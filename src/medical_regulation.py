218b7910c9939d9f178ecdcb32d127028bb1234f
Fatoumata Badji <60388963+Fatoumata964@users.noreply.github.com>
2024-05-27 11:39:05 +0200

    Update medical_regulation.py

--- src/medical_regulation.py
+++ src/medical_regulation.py
@@ -4,7 +4,6 @@ from colabcode import ColabCode
 from fastapi import FastAPI
 from pydantic import BaseModel
 from faiss_M import faiss_search_similar_medications
-from bioSentVec import *
 from llm import mistral_llm
 from ner import train
 import pickle
